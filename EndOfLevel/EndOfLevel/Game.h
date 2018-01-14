05a16693c522444750267d18f15bc47ded2863e4
Kevin Boylan <idakev2396@gmail.com>
2018-01-14 15:05:00 +0000

    Basic Setup + set up directories using macro

---
+++ EndOfLevel/EndOfLevel/Game.h
@@ -0,0 +1,48 @@
+#pragma once
+//#include <SDL.h>
+//#include <SDL_image.h>
+#include <iostream>
+
+#define fps 60
+#define window_width 1000
+#define window_height 800
+
+
+class Game {
+private:
+	bool isRunning;
+	//SDL_Window* window = nullptr;
+
+	////const int WALKING_ANIMATION_FRAMES = 10;
+	////SDL_Rect sheetRect[10];
+	//// used for rendering the current frame
+	//SDL_Texture* texture = nullptr;
+	//SDL_Renderer* renderer = nullptr;
+	//SDL_Rect* currentRect;
+
+	//SDL_Surface *screen;
+	//Uint32 white;
+	//Uint32 starting_tick;
+
+	//int frame = 0;
+
+	//void FramerateHandler();
+
+	//Animation fsm;
+	//State s;
+
+public:
+	/*void Initialize();
+	void HandleEvents();
+
+	void FrameHandler();*/
+
+	void Update() {};
+	void Render() {};
+	void CleanUp() {};
+
+	bool IsRunning()
+	{
+		return isRunning;
+	};
+};
